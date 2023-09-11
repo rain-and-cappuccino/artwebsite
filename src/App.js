0e511193107aac30cb8152ba5870aac76197c92e
crow <crowpaintsthings@gmail.com>
2023-09-10 23:26:27 -0400

    changes for smaller screens

--- src/App.js
+++ src/App.js
@@ -10,12 +10,11 @@ import './layouts/Gallery.css';
 function App(props) {
     console.log(props);
     return (
-        // className="Gallery" probably unintentional, "Gallery" is already being used below
-        <div>
+        <div className="GalleryPage">
             <div className="Sidenavi">
                 <Sidenavi
                     imgsrc={WinterCamping}
-                    name="Winter Camping"
+                    name="FirstName LastName"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at aliquam justo.
                     Cras eu lobortis elit, eu egestas velit. Donec diam augue, congue non volutpat eget,
                     feugiat ultrices elit. Fusce sed lectus cursus, porta sem id, ultricies odio. Vivamus
@@ -25,6 +24,30 @@ function App(props) {
             <div className="GalleryContainer">
                 <div className="Gallery">
                 {/* className="ImageCard" is already in the component */}
+                    <ImageCard
+                        imgsrc={Snow}
+                        title="Snow"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at aliquam justo.
+                        Cras eu lobortis elit, eu egestas velit. Donec diam augue, congue non volutpat eget,
+                        feugiat ultrices elit. Fusce sed lectus cursus, porta sem id, ultricies odio. Vivamus
+                        dictum nibh ultricies sapien condimentum, sed feugiat massa venenatis."
+                    />
+                    <ImageCard
+                        imgsrc={CupofTea}
+                        title="Cup of Tea"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at aliquam justo.
+                        Cras eu lobortis elit, eu egestas velit. Donec diam augue, congue non volutpat eget,
+                        feugiat ultrices elit. Fusce sed lectus cursus, porta sem id, ultricies odio. Vivamus
+                        dictum nibh ultricies sapien condimentum, sed feugiat massa venenatis."
+                    />
+                    <ImageCard
+                        imgsrc={JapaneseSpitz}
+                        title="Japanese Spitz"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at aliquam justo.
+                        Cras eu lobortis elit, eu egestas velit. Donec diam augue, congue non volutpat eget,
+                        feugiat ultrices elit. Fusce sed lectus cursus, porta sem id, ultricies odio. Vivamus
+                        dictum nibh ultricies sapien condimentum, sed feugiat massa venenatis."
+                    />
                     <ImageCard
                         imgsrc={Snow}
                         title="Snow"
--- src/components/Sidenavi.js
+++ src/components/Sidenavi.js
@@ -4,8 +4,10 @@ export default function Sidenavi(props) {
     console.log(props);
     return (
         <div className="Sidenavi">
-            <img className='Sidenavi-pic' src={props.imgsrc} alt="img" />
-            <h1>{props.title}</h1>
+            <div className="Sidenavi-heading">
+                <img className='Sidenavi-pic' src={props.imgsrc} alt="img" />
+                <h1>{props.name}</h1>
+            </div>
             <p>{props.description}</p>
         </div>
     )
